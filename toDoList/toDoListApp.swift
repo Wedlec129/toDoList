63fdb654c78d335462cdab627f43eb25f58c6f84
Wedlec129 <boryasokolov@gmail.com>
2023-08-09 13:05:37 +0300

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Борух Соколов on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
